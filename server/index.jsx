4752b55783f142319f71837abd3dd2448e7296cc
PyColors <patweb44@gmail.com>
2018-09-06 23:55:13 +0100

    Adding API Mock and Real Data by redux-saga

--- server/index.jsx
+++ server/index.jsx
@@ -2,10 +2,32 @@ import express from 'express';
 import yields from 'express-yields';
 import fs from 'fs-extra';
 import webpack from 'webpack';
-
+import { argv } from 'optimist';
+import { get } from 'request-promise';
+import { questions, question } from '../data/api-real-url';
+import { delay } from 'redux-saga';
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const useLiveData = argv.useLiveData === "true";
+
+function * getQuestion() {
+    let data;
+    if (useLiveData) {
+        data = yield get (questions, {gzip: true});
+    } else {
+        data = yield fs.readFile('./data/mock-questions.json', "utf-8");
+    }
+
+    return JSON.parse(data);
+}
+
+app.get('/api/questions', function * (req, res) {
+    const data = yield getQuestion();
+    yield delay(150);
+    res.json(data);
+});
 
 if (process.env.NODE_ENV === 'development') {
     // Import statement
