4a99f48ebeaeef937cf32a0ed73c5717112f0227
PyColors <patweb44@gmail.com>
2018-09-07 18:01:46 +0100

    Adding React and render the App with

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const AppDisplay = () => (
+    <div>
+        <h1>Isomorphic React App</h1>
+    </div>
+);
+
+export default AppDisplay;
--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,12 @@
-console.log('test webpack');
+import App from './App';
+import ReactDom from 'react-dom';
+import React from 'react';
+
+const render = (_App) => {
+    ReactDom.render(
+        <_App />,
+        document.getElementById('AppContainer')
+    )
+};
+
+render(App);
