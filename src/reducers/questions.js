42e5461a8423eff5c49ae26648a7f82fa809de11
PyColors <patweb44@gmail.com>
2018-09-07 23:42:54 +0100

    Fixed Reducer bug to see the next state

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -1,12 +1,12 @@
-import unionWith from 'lodash/unionWith';
+import unionWith from "lodash/unionWith";
 
-export const questions = (state = [], {type, question})=>{
-    const questionEquality = (a = {}, b = {})=>{
-        return a.question_id == b.question_id
+export const questions = (state = [], { type, questions }) => {
+    const questionEquality = (a = {}, b = {}) => {
+        return a.question_id == b.question_id;
     };
 
-    if (type === `FETCHED_QUESTION`) {
-        state = unionWith([question],state,questionEquality);
+    if (type === `FETCHED_QUESTIONS`) {
+        state = unionWith(state, questions, questionEquality);
     }
 
     return state;
