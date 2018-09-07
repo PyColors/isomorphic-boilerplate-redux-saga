c037c0e1e714f612013969e09125fd292004a495
PyColors <patweb44@gmail.com>
2018-09-07 17:31:12 +0100

    Adding Routes to Real API and Mock Data

--- server/index.jsx
+++ server/index.jsx
@@ -12,19 +12,44 @@ const app = express();
 
 const useLiveData = argv.useLiveData === "true";
 
-function * getQuestion() {
+function * getQuestions (){
     let data;
     if (useLiveData) {
-        data = yield get (questions, {gzip: true});
+        data = yield get(questions,{gzip:true});
     } else {
-        data = yield fs.readFile('./data/mock-questions.json', "utf-8");
+        data = yield fs.readFile('./data/mock-questions.json',"utf-8");
     }
 
     return JSON.parse(data);
 }
 
-app.get('/api/questions', function * (req, res) {
-    const data = yield getQuestion();
+function * getQuestion (question_id) {
+    let data;
+    if (useLiveData) {
+        data = yield get(question(question_id),{gzip:true,json:true});
+    } else {
+        const questions = yield getQuestions();
+        const question = questions.items.find(_question=>_question.question_id == question_id);
+        question.body = `Mock question body: ${question_id}`;
+        data = {items:[question]};
+    }
+
+    return data;
+}
+
+app.get('/api/questions',function *(req,res){
+    const data = yield getQuestions();
+    /**
+     * Small delay - async/hot-reloading aspects
+     * Strongly to remove for production.
+     */
+    yield delay(150);
+    res.json(data);
+});
+
+app.get('/api/questions/:id',function *(req,res){
+    const data = yield getQuestion(req.params.id);
+     // Remove this delay for production.
     yield delay(150);
     res.json(data);
 });
