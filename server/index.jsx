c3d0a83e097c4a0d51fce3572c20620f41e2465a
PyColors <patweb44@gmail.com>
2018-09-08 21:09:08 +0100

    Adding Production settings

--- server/index.jsx
+++ server/index.jsx
@@ -13,6 +13,7 @@ import React from 'react';
 import App from '../src/App';
 import { ConnecteRouter } from 'react-router-redux';
 import createHistory from 'history/createMemoryHistory';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -72,6 +73,8 @@ if (process.env.NODE_ENV === 'development') {
     }));
 
     app.use(require('webpack-hot-middleware')(compiler));
+} else {
+    app.use(express.static(path.resolve(__dirname, '../dist')));
 }
 
 app.get(['/', 'questions/:id'], function * (req, res) {
