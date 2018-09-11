15234d56cea59cebd249541ade860111cd2e6d53
Parny P <patrice.parny@vodafone.com>
2018-09-12 00:10:28 +0100

    Fix bug to Jest

--- src/sagas/fetch-question-saga.spec.js
+++ src/sagas/fetch-question-saga.spec.js
@@ -1,14 +1,16 @@
-import { handleFetchQuestion } from './fetch-question-saga';
+import { handleFetchQuestion }  from './fetch-question-saga';
 import fetch from 'isomorphic-fetch';
 
 describe("Fetch question saga", () => {
+
     beforeAll(() => {
         fetch.__serValue([{question_id:42}]);
     });
+
     it("should fetch the questions", async () => {
         const gen = handleFetchQuestion({question_id:42});
         const { value } = await gen.next();
-        expect(value).toEquel([{questions:42}]);
+        expect(value).toEqual([{questions:42}]);
         expect(fetch).toHaveBeenCalledWith(`/api/questions/42`)
     });
 });
