4006968461aa72d53f69648056026d05c53656d6
PyColors <patweb44@gmail.com>
2018-09-12 21:56:38 +0100

    Implementting React Redux Container Testing by export from <Component

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,4 +1,31 @@
+import { mapStateToProps } from '../QuestionDetail'
+import renderer from 'react-test-renderer';
+import React from 'react';
+
 describe(`The Question Detail <Component`, () => {
+    describe(`The Container Element`, () => {
+        /**
+         * As Map State to Props is a pure function, it's lovely test it 😎
+         * without without adding libraries.
+         */
+        describe(`mapStateToProps`, () => {
+            it("should map the state to props correctly", () => {
+                const sampleQuestion = {
+                    question_id:42,
+                    body:"Lorem Ipsum"
+                };
+                const appState = {
+                    questions: [sampleQuestion]
+                };
+                const ownProps = {
+                    question_id:42,
+                };
+                const componentState = mapStateToProps(appState, ownProps);
+                expect(componentState).toEqual(sampleQuestion);
+            });
+        });
+    });
+
     it(`Should not regress`, () => {
 
     });
