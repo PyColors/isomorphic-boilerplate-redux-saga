1cc8a0a616d41c6716467ffcdb05fd500961dfd4
PyColors <patweb44@gmail.com>
2019-03-20 22:18:25 +0000

    Setting useLiveData useServerRender under config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,11 @@
+import { argv } from 'optimist';
+
 const config = {
   http: {
     port: process.env.PORT || 3000
-  }
+  },
+  useLiveData: argv.useLiveData === 'true',
+  useServerRender: argv.ServerRender === 'true'
 };
 
 export default config;
